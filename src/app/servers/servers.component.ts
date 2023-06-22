5b8805eb56d156b484d23edf013627a1e401cca1
MorsaMoradi <MorsaMoradi@gmail.com>
2023-06-22 11:38:01 +0330

    peroperty binding - 028 exercise

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector:'app-servers',
+  selector: 'app-servers',
   // selector: '.app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>` ,
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>` ,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
-
+  allowNewServer = false;
+  constructor() {
+    setTimeout(() => { this.allowNewServer = !this.allowNewServer; }, 2000);
+  }
 }
